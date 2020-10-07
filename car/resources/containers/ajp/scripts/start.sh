dc958004a7d4de6bb815fa005de9019e3f5daea3
TNeitzel <qtc_de@outlook.com>
2020-10-07 08:18:08 +0200

    Update ajp container
    Updated the ajp container. The container uses now a fixed version of
    alpine and installs apache2 manually. This way, the overall container
    size could be reduced from 200MB to about 10MB. The only downside is
    that mod_jk.so now need to be compiled manually. However, a script was
    added to do this automatically and also a precompiled version was
    included.

--- car/resources/containers/ajp/scripts/start.sh
+++ car/resources/containers/ajp/scripts/start.sh
@@ -1,30 +1,25 @@
-#!/bin/bash
+#!/bin/sh
 
 set -e
 
 if [ -z ${HOST} ]; then
-
   echo "[-] Error. Environment variable 'HOST' needs to be set."
   echo "[-] Set the 'HOST' environment variable to the targeted AJP server."
   exit 1
-
 fi
 
 if [ -z ${PORT} ]; then
-
   echo "[-] Error. Environment variable 'PORT' needs to be set."
   echo "[-] Set the 'PORT' environment variable to the targeted port on the AJP server."
   exit 1
-
 fi
 
-echo "[+] Adjusting host and port values inside the jk_workes properties file"
-cp /usr/local/apache2/conf/extra/jk_workers.properties /usr/local/apache2/conf/extra/jk_workers_local.properties
-sed -i -e "s/<@:HOST:@>/${HOST}/" /usr/local/apache2/conf/extra/jk_workers_local.properties
-sed -i -e "s/<@:PORT:@>/${PORT}/" /usr/local/apache2/conf/extra/jk_workers_local.properties
+echo "[+] Adjusting host and port values inside the jk_workes.properties file."
+sed -i -e "s/<@:HOST:@>/${HOST}/" /etc/apache2/jk_workers.properties
+sed -i -e "s/<@:PORT:@>/${PORT}/" /etc/apache2/jk_workers.properties
 
 echo "[+] Adjusting volume permissions."
-chown 1000:1000 /usr/local/apache2/logs
+chown 1000:1000 /var/log/apache2
 
 echo "[+] Starting AJP proxy server."
-httpd-foreground
+httpd -DFOREGROUND

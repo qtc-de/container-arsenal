7884ee9c11c626c601cdd5df9ccf5a9ae7ae4585
TNeitzel <qtc_de@outlook.com>
2020-10-11 07:19:04 +0200

    Update flask container
    Added env_info and updated the h2b.py script to work with ssl
    connections.

--- car/resources/containers/h2b/scripts/h2b.py
+++ car/resources/containers/h2b/scripts/h2b.py
@@ -1,45 +1,98 @@
+import ssl
 import socket
+
 from flask import Flask, request
 
 app = Flask(__name__)
 
 
+class InvalidArgsException(Exception):
+    '''
+    Custom exception class.
+    '''
+
+
+def get_args(request):
+    '''
+    Parses the specified arguments during a POST request. It verifies whether the remote
+    host and port arguments were set, whether the request body contains content and whether
+    SSL connections should be used.
+
+    Paramaters:
+        request             (request)               Incoming HTTP request
+
+    Returns:
+        remote              (list[string])          Remote host, port, content and ssl setting
+    '''
+    host = request.args.get('host')
+    port = request.args.get('port', type=int)
+    use_ssl = request.args.get('ssl', default=False)
+
+    content = request.get_data()
+
+    if host is None:
+        raise InvalidArgsException("Error! GET parameter 'host' needs to be specified.\n")
+
+    if port is None:
+        raise InvalidArgsException("Error! GET parameter 'port' needs to be specified.\n")
+
+    if content == b'':
+        raise InvalidArgsException("No HTTP body found. Nothing to forward.\n")
+
+    return [host, port, content, use_ssl]
+
+
 @app.route('/')
 def server_status():
+    '''
+    When just querying the root of the server, we return a static banner.
+
+    Paramaters:
+        None
+
+    Returns:
+        banner              (string)                Server banner
+    '''
     return 'Proxy is ready!\n'
 
 
 @app.route('/forward', methods=['GET', 'POST'])
 def forward():
+    '''
+    Forward the POST body of a request to the specified remote host and return
+    the result. The remote host, content and ssl option are all specified via
+    URL paramaters.
 
-    host = request.args.get('host')
-    port = request.args.get('port', type=int)
-    content = request.get_data()
+    Paramaters:
+        None
 
-    if host is None:
-        return "Error! GET parameter 'host' needs to be specified.\n"
+    Returns:
+        content             (string)                Content received by the remote target
+    '''
 
-    if port is None:
-        return "Error! GET parameter 'port' needs to be specified.\n"
+    try:
+        host, port, content, use_ssl = get_args(request)
 
-    if content == b'':
-        return "No HTTP body found. Nothing to forward.\n"
+    except InvalidArgsException as e:
+        return str(e)
 
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    if use_ssl:
+        s = ssl.wrap_socket(s, cert_reqs=ssl.CERT_NONE)
 
-        s.settimeout(10)
-        target = (host, port)
+    s.settimeout(10)
+    target = (host, port)
 
-        try:
-            s.connect(target)
-            s.sendall(content)
+    try:
+        s.connect(target)
+        s.sendall(content)
 
-            master_data = b''
-            while True:
-                data = s.recv(1024)
-                if len(data) <= 0:
-                    return master_data
-                master_data += data
+        master_data = b''
+        while True:
+            data = s.recv(1024)
+            if len(data) <= 0:
+                return master_data
+            master_data += data
 
-        except ConnectionRefusedError:
-            return f"Error your target '{host}:{port}' refused the connection\n"
+    except ConnectionRefusedError:
+        return f"Target '{host}:{port}' refused the connection\n"

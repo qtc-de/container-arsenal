74ce8de294639abc1c63b51f851185d097bf08a8
TNeitzel <qtc_de@outlook.com>
2020-10-14 06:51:39 +0200

    Update samba container

--- car/resources/containers/samba/scripts/start.sh
+++ car/resources/containers/samba/scripts/start.sh
@@ -1,11 +1,9 @@
 #!/bin/ash
 
 if [ -z ${PASSWORD} ]; then
-
   PASSWORD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
   echo "[+] No password was specified."
   echo "[+] Generated random password for user 'default': ${PASSWORD}"
-
 fi
 
 echo -e "${PASSWORD}\n${PASSWORD}" | smbpasswd -a -s -c /config/smb.conf default &> /dev/null
@@ -17,7 +15,7 @@ chmod 775 /share/private
 chmod 775 /share/public
 
 # If we need additional users with fixed passwords we can use the following command:
-#add-smb-user "test" "test"
+#add-smb-user "user" "password"
 
 echo "[+] Starting samba service."
-supervisord -c "/config/supervisord.conf"
+smbd --foreground --no-process-group --log-stdout --configfile /config/smb.conf

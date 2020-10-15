f60e2f0381763b855609a49c7154ab1f6fe93cc6
TNeitzel <qtc_de@outlook.com>
2020-10-15 09:09:46 +0200

    Make ajp container uid independent

--- car/resources/containers/ajp/scripts/start.sh
+++ car/resources/containers/ajp/scripts/start.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+if [ -z ${LOCAL_UID} ]; then
+    LOCAL_UID=1000
+fi
+
 if [ -z ${TARGET_HOST} ]; then
   echo "[-] Error. Environment variable 'HOST' needs to be set."
   echo "[-] Set the 'HOST' environment variable to the targeted AJP server."
@@ -22,7 +26,7 @@ echo "[+] Adjusting listening port in httpd.conf."
 sed -e "s/<@:PORT:@>/${LISTEN_PORT}/" /etc/apache2/httpd_raw.conf > /etc/apache2/httpd.conf
 
 echo "[+] Adjusting volume permissions."
-chown 1000:1000 /var/log/apache2
+chown ${LOCAL_UID}:${LOCAL_UID} /var/log/apache2
 
 echo "[+] Starting AJP proxy server."
 httpd -DFOREGROUND

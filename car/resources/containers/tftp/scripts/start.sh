3779b589bfb89939185c1db05071901a29335149
TNeitzel <qtc_de@outlook.com>
2020-10-20 07:55:55 +0200

    Add basic logging to tftp container
    It seems impossible to configure tftp-hpa with logging (at least I did
    not found any configuration that worked. Even in verbose mode, no logs
    are written. Maybe logging only works in combination with inetd). To get
    at least some information on uploaded files, a simple logger was
    included into the startup script.

--- car/resources/containers/tftp/scripts/start.sh
+++ car/resources/containers/tftp/scripts/start.sh
@@ -15,4 +15,16 @@ echo "[+] Adjusting volume permissions."
 chown default:default /ftp
 
 echo "[+] Starting tftpd."
-in.tftpd --foreground --create -m /config/mapfile --user default --secure /ftp --verbose --address "0.0.0.0:${LISTEN_PORT}"
+in.tftpd --listen --create -m /config/mapfile --user default --secure /ftp --verbose --address "0.0.0.0:${LISTEN_PORT}" 
+
+LIST_CMD='find /ftp -type f | xargs ls -ld'
+echo "[+] Current server contents:"
+eval $LIST_CMD | tee /tmp/files_old
+
+echo "[+] New uploaded files:"
+while true; do
+    sleep 5
+    eval $LIST_CMD > /tmp/files_new
+    diff /tmp/files_old /tmp/files_new | grep -E "^\+[^+]" | cut -f2 -d"+"
+    cp /tmp/files_new /tmp/files_old
+done

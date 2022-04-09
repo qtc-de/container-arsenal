b35f2138b214b6a766ddbd2f06e18f07ead8abbd
TNeitzel <qtc_de@outlook.com>
2022-04-09 22:06:10 +0200

    Update ajp container

--- car/resources/containers/ajp/scripts/build_modjk.sh
+++ car/resources/containers/ajp/scripts/build_modjk.sh
@@ -11,7 +11,7 @@ echo "[+] Installing dependencies..."
     apk add apr-util-dev
 
 echo "[+] Downloading mod_jk connector..."
-    wget https://ftp-stud.hs-esslingen.de/pub/Mirrors/ftp.apache.org/dist/tomcat/tomcat-connectors/jk/tomcat-connectors-1.2.48-src.tar.gz
+    wget 'https://dlcdn.apache.org/tomcat/tomcat-connectors/jk/tomcat-connectors-1.2.48-src.tar.gz'
     hash=$(sha512sum tomcat-connectors-1.2.48-src.tar.gz | cut -f1 -d" ")
     if [[ "$hash" != "955a830724a3902e29032a5d2e7603d3170334e8a383d314f6bf8539d53d9f7ee4cfa0b31cfc954acb0a13d9975ed2229de085d08de3885f8679b509924fde47" ]]; then
         echo "[-] Hash Mismatch!";

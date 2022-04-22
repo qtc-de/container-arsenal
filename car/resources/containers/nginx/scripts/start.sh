26b67bd02fe787c7a6b8a0500b6a763d02e29c67
TNeitzel <qtc_de@outlook.com>
2022-04-22 07:44:13 +0200

    [nginx] Create TLS certificate dynamically
    Replaced the statically created TLS certificate with a dynamically
    created one.

--- car/resources/containers/nginx/scripts/start.sh
+++ car/resources/containers/nginx/scripts/start.sh
@@ -7,8 +7,16 @@ fi
 echo "[+] Adjusting UID values."
 usermod -u ${LOCAL_UID} nginx &> /dev/null
 groupmod -g ${LOCAL_UID} nginx &> /dev/null
-chown nginx:nginx /var/www/html/download
-chown nginx:nginx /var/www/html/upload
+
+echo "[+] Adjusting volume permissions."
+chown -R -P nginx:nginx /var/www/html
+
+if ! [ -f /etc/nginx/ssl/webdav-cert.pem ]; then
+    echo "[+] Creating TLS certificate."
+    openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 \
+        -subj "/C=ME/ST=TheShire/L=Frogmorton/O=LOTR/OU=Hobbits/CN=webdav-server" \
+        -keyout /etc/nginx/ssl/webdav-key.pem  -out /etc/nginx/ssl/webdav-cert.pem &> /dev/null
+fi
 
 if [ -z ${PASSWORD} ]; then
   PASSWORD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
